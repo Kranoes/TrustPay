bfadd77e4ab690d600483261a8acd5a47be85664
Кирилл Черников <kirill.chernikowa@gmail.com>
2026-08-10 12:58:39 +0300

    refactor(wallets): доработка бизнес-логики кошелька и списывания средств

---
+++ src/TrustPay.Application/Wallets/Commands/UnfreezeWallet/UnfreezeWalletCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+
+namespace TrustPay.Application.Wallets.Commands.UnfreezeWallet
+{
+    public class UnfreezeWalletCommandValidator : AbstractValidator<UnfreezeWalletCommand>
+    {
+        public UnfreezeWalletCommandValidator()
+        {
+            RuleFor(x => x.WalletId).NotEmpty().WithMessage("Id кошелька обязателен.");
+        }
+    }
+}
