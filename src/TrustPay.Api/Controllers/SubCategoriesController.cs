ea600416a462f4526ac41da25843e138cadcffb5
Кирилл Черников <kirill.chernikowa@gmail.com>
2026-08-11 11:52:05 +0300

    feat(subcategories): implement CRUD, CQRS handlers, validators, and controller

---
+++ src/TrustPay.Api/Controllers/SubCategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrustPay.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubCategoriesController : ControllerBase
+    {
+    }
+}
---
+++ src/TrustPay.Application/SubCategories/Queries/GetSubCategoriesByCategory/GetSubCategoriesByCategoryIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrustPay.Application.SubCategories.Queries.GetSubCategoriesByCategory
+{
+    internal class GetSubCategoriesByCategoryIdQuery
+    {
+    }
+}
