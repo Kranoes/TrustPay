4bd875b9466181c556a2b96ef16a3bf90d798cad
Кирилл Черников <kirill.chernikowa@gmail.com>
2026-09-11 19:09:09 +0300

    feat:added uni tests for Domain

--- TrustPay.UnitTests/Domain/Entities/WalletTests.cs
+++ TrustPay.UnitTests/Domain/Entities/WalletTests.cs
@@ -1,10 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace TrustPay.UnitTests.Domain.Entities;
 
-namespace TrustPay.UnitTests.Domain.Entities
+using FluentAssertions;
+using TrustPay.Domain.Entities;
+using TrustPay.Domain.ValueObjects;
+using Xunit;
+
+public class WalletTests
 {
-    internal class WalletTests
+    [Fact]
+    public void Deposit_ShouldIncreaseBalance_WhenAmountIsValid()
     {
+        var userId = Guid.NewGuid();
+        var initialBalance = Money.Create(0m, "RUB").Value;
+        var wallet = Wallet.Create(userId, initialBalance).Value;
+        var depositAmount = Money.Create(500m, "RUB").Value;
+
+        var result = wallet.Deposit(depositAmount);
+
+        result.IsSuccess.Should().BeTrue();
+        wallet.AvailableBalance.Amount.Should().Be(500m);
+    }
+
+    [Fact]
+    public void Withdraw_ShouldDecreaseBalance_WhenFundsAreSufficient()
+    {
+        var userId = Guid.NewGuid();
+        var initialBalance = Money.Create(1000m, "RUB").Value;
+        var wallet = Wallet.Create(userId, initialBalance).Value;
+        var withdrawAmount = Money.Create(400m, "RUB").Value;
+
+        var result = wallet.Withdraw(withdrawAmount);
+
+        result.IsSuccess.Should().BeTrue();
+        wallet.AvailableBalance.Amount.Should().Be(600m);
+    }
+
+    [Fact]
+    public void Withdraw_ShouldReturnFailure_WhenInsufficientFunds()
+    {
+        var userId = Guid.NewGuid();
+        var initialBalance = Money.Create(50m, "RUB").Value;
+        var wallet = Wallet.Create(userId, initialBalance).Value;
+        var withdrawAmount = Money.Create(100m, "RUB").Value;
+
+        var result = wallet.Withdraw(withdrawAmount);
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Description.Should().Be("Недостаточно средств.");
+        wallet.AvailableBalance.Amount.Should().Be(50m);
     }
 }
--- TrustPay.UnitTests/Domain/ValueObjects/MoneyTests.cs
+++ TrustPay.UnitTests/Domain/ValueObjects/MoneyTests.cs
@@ -1,10 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace TrustPay.UnitTests.Domain.ValueObjects;
 
-namespace TrustPay.UnitTests.Domain.ValueObjects
+using FluentAssertions;
+using TrustPay.Domain.ValueObjects;
+using Xunit;
+
+public class MoneyTests
 {
-    internal class MoneyTests
+    [Fact]
+    public void Create_ShouldReturnSuccess_WhenAmountAndCurrencyAreValid()
     {
+        var amount = 100m;
+        var currency = "RUB";
+
+        var result = Money.Create(amount, currency);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Amount.Should().Be(amount);
+        result.Value.Currency.Should().Be("RUB");
+    }
+
+    [Fact]
+    public void Create_ShouldReturnFailure_WhenAmountIsNegative()
+    {
+        var result = Money.Create(-100m, "RUB");
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Description.Should().Be("Сумма должна быть больше 0");
+    }
+
+    [Fact]
+    public void Add_ShouldIncreaseAmount_WhenCurrenciesMatch()
+    {
+        var money1 = Money.Create(100m, "RUB").Value;
+        var money2 = Money.Create(50m, "RUB").Value;
+
+        var result = money1.Add(money2);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Amount.Should().Be(150m);
+        result.Value.Currency.Should().Be("RUB");
+    }
+
+    [Fact]
+    public void Add_ShouldReturnFailure_WhenCurrenciesMismatch()
+    {
+        var money1 = Money.Create(100m, "RUB").Value;
+        var money2 = Money.Create(50m, "USD").Value;
+
+        var result = money1.Add(money2);
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Description.Should().Contain("Нельзя складывать разные валюты");
+    }
+
+    [Fact]
+    public void Subtract_ShouldReturnSuccess_WhenFundsAreSufficient()
+    {
+        var money1 = Money.Create(100m, "RUB").Value;
+        var money2 = Money.Create(40m, "RUB").Value;
+
+        var result = money1.Subtract(money2);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Amount.Should().Be(60m);
+    }
+
+    [Fact]
+    public void Subtract_ShouldReturnFailure_WhenInsufficientFunds()
+    {
+        var money1 = Money.Create(30m, "RUB").Value;
+        var money2 = Money.Create(50m, "RUB").Value;
+
+        var result = money1.Subtract(money2);
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Description.Should().Be("Недостаточно средств.");
     }
 }
